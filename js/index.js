728ebe15502e9fbb9af996049c3e01a7c9be5d2e
gallou <loic.gallou@etu.univ-amu.fr>
2023-12-20 15:16:43 +0100

    mode telephone

--- js/index.js
+++ js/index.js
@@ -2,19 +2,5 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
   }
-  var sidenav = document.getElementById("mySidenav");
-var openBtn = document.getElementById("openBtn");
-var closeBtn = document.getElementById("closeBtn");
 
-openBtn.onclick = openNav;
-closeBtn.onclick = closeNav;
-
-/* Set the width of the side navigation to 250px */
-function openNav() {
-  sidenav.classList.add("active");
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-  sidenav.classList.remove("active");
 }
