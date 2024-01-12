c7ef67829505d9b07b0baf81e04ce20dfff6c111
gallou <loic.gallou@etu.univ-amu.fr>
2024-01-12 20:10:17 +0100

    mode telephone

--- js/hotland.js
+++ js/hotland.js
@@ -8,7 +8,13 @@ let mybutton = document.getElementById("myBtn");
 
 window.onscroll = function() {scrollFunction()};
 
-
+function scrollFunction() {
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  mybutton.style.display = "block";
+} else {
+  mybutton.style.display = "none";
+}
+}
 
 
 function topFunction() {
